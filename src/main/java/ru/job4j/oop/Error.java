007ef109e9c4859ed5ed5be4873ecb5a98f29160
Lalina Kairova <klea0840@gmail.com>
2021-03-28 14:42:34 +0400

    1.7. Конструктор [#488692]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.message = "Deafault";
+        this.status = 1;
+        this.active = true;
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+        System.out.println("Active: " + active);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        Error definedError = new Error(false, 0, "Not working");
+        defaultError.printInfo();
+        System.out.println();
+        definedError.printInfo();
+    }
+}
