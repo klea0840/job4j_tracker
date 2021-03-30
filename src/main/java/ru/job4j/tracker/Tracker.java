a64f27c44f34adef19b5c098d85a5ec1e7d943a6
Lalina Kairova <klea0840@gmail.com>
2021-03-30 23:49:47 +0400

    6. Метод замены заявки. Tracker.replace [#488709]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,18 +14,20 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+//        Item rsl = null;
+//        for (int index = 0; index < size; index++) {
+//            Item item = items[index];
+//            if (item.getId() == id) {
+//                rsl = item;
+//                break;
+//            }
+//        }
+//        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
-    public  Item[] findAll(Item[] items) {
+    public Item[] findAll(Item[] items) {
         Item[] newItems = new Item[items.length];
         int size = 0;
         for (int i = 0; i < items.length; i++) {
@@ -34,11 +36,11 @@ public class Tracker {
                 newItems[size] = name;
                 size++;
             }
-            }
+        }
         return Arrays.copyOf(newItems, size);
     }
 
-    public  Item[] findByName(String key) {
+    public Item[] findByName(String key) {
         Item[] newItems = new Item[items.length];
         int size = 0;
         for (int i = 0; i < items.length; i++) {
@@ -49,5 +51,27 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(newItems, size);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item x) {
+        boolean rsl = false;
+        if (indexOf(id) != -1) {
+            int index = indexOf(id);
+            x.setId(id);
+            items[index] = x;
+            rsl = true;
+        }
+        return rsl;
     }
 }
