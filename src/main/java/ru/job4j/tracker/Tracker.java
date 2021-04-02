6427262780b9a3487039866a1a388121bbc26887
Lalina Kairova <klea0840@gmail.com>
2021-04-02 13:56:28 +0400

    5. Tracker - хранилище [#488714]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,16 +28,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] newItems = new Item[items.length];
-        int size = 0;
-        for (int i = 0; i < items.length; i++) {
-            Item name = items[i];
-            if (name != null) {
-                newItems[size] = name;
-                size++;
-            }
-        }
-        return Arrays.copyOf(newItems, size);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
