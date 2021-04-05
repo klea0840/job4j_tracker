821a79cb926f4916a7856a25d830b4a6d6d3b39a
Lalina Kairova <klea0840@gmail.com>
2021-04-05 10:55:40 +0400

    5.1. Тестирование. Подготовка данных. [#488643]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class StartUITest extends TestCase {
+public class StartUITest {
     @Test
     public void testCreate3Item() {
         String[] answers = {"Fix PC", "Item2", "25"};
@@ -19,7 +19,7 @@ public class StartUITest extends TestCase {
         assertThat(created.getName(), is(expected.getName()));
     }
 
-
+    @Test
     public void testEditItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
@@ -28,25 +28,25 @@ public class StartUITest extends TestCase {
         tracker.add(item2);
         String[] answers = {
                 String.valueOf(item2.getId()), /* id сохраненной заявки в объект tracker. */
-                String.valueOf(item.getName()), /* id сохраненной заявки в объект tracker. */
+                item.getName() /* id сохраненной заявки в объект tracker. */
         };
         StartUI.editItem(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item2.getId());
         assertThat(replaced.getName(), is(item.getName()));
     }
 
-
+    @Test
     public void testDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
-        Item item2 = new Item("news item");
+        Item newIitem = new Item("news item");
         tracker.add(item);
-        tracker.add(item2);
+        tracker.add(newIitem);
         String[] answers = {
-                String.valueOf(item2.getId()), /* id сохраненной заявки в объект tracker. */
+                String.valueOf(newIitem.getId()), /* id сохраненной заявки в объект tracker. */
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item2.getId());
+        Item deleted = tracker.findById(newIitem.getId());
         assertNull(deleted);
     }
 }
