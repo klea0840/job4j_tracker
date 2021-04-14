0c412b6d63e40a4136aff1b20c62d2ae054d610b
Lalina Kairova <klea0840@gmail.com>
2021-04-14 19:51:57 +0400

    2. Очередь с приоритетом на LinkedList [#488614]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
