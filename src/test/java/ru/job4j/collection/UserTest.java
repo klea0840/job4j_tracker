0fb2fb07191c41af702aa7b013e4c10c13c9500a
Lalina Kairova <klea0840@gmail.com>
2021-04-29 00:46:30 +0400

    1. Организовать сортировку User [#488609]

--- src/test/java/ru/job4j/collection/UserTest.java
+++ src/test/java/ru/job4j/collection/UserTest.java
@@ -14,16 +14,16 @@ public class UserTest {
     @Test
     public void whenAsc() {
         Set<User> users = new TreeSet<>();
-        users.add(new User("Petr", 32));
+        users.add(new User("Petr", 45));
         users.add(new User("Ivan", 31));
         Iterator<User> it = users.iterator();
         assertThat(it.next(), is(new User("Ivan", 31)));
-        assertThat(it.next(), is(new User("Petr", 32)));
+        assertThat(it.next(), is(new User("Petr", 45)));
     }
 
     @Test
     public void whenComparePertVSIvan() {
-        int rsl = new User("Petr", 32)
+        int rsl = new User("Petr", 45)
                 .compareTo(
                         new User("Ivan", 31)
                 );
