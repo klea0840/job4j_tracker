be88bc001ff3cf36bc4cbd7fbe5a71437b162cd1
Lalina Kairova <klea0840@gmail.com>
2021-05-27 22:10:49 +0300

    0. Stream API [#58166 #161398] corr

--- src/main/java/ru/job4j/stream/StreamNumbers.java
+++ src/main/java/ru/job4j/stream/StreamNumbers.java
@@ -1,23 +1,16 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamNumbers {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(-20);
-        numbers.add(-7);
-        numbers.add(450);
-        numbers.add(0);
-        numbers.add(64);
-        numbers.add(9);
-        numbers.add(10);
-
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(-20, -7, 450, 0, 64, 9, 10));
         List<Integer> sortedNumbers = numbers.stream().filter(
                 num -> num > 0
         ).collect(Collectors.toList());
-        sortedNumbers.forEach(System.out :: println);
+//        sortedNumbers.forEach(System.out::println);
     }
 }
