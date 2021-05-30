76d75cbc7e29571b262ecd692230eba7bc80736d
Lalina Kairova <klea0840@gmail.com>
2021-05-30 17:15:26 +0300

    4. Преобразование List в Map. [#110226]

---
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+
+    public Map<String, Student> toMap(List<Student> list) {
+        return list.stream().distinct().collect(
+                Collectors.toMap(
+                        Student::getSurname,
+                        s -> s
+                ));
+    }
+}
---
+++ src/test/java/ru/job4j/stream/ListToMapTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ListToMapTest {
+    @Test
+    public void testToMap() {
+        Student student1 = new Student(10, "Surname1");
+        Student student2 = new Student(40, "Surname4");
+        Student student3 = new Student(50, "Surname5");
+        Student student4 = new Student(50, "Surname5");
+        List<Student> students = List.of(
+                student1, student2, student3, student4
+        );
+        ListToMap newMap = new ListToMap();
+        Map<String, Student> rsl = newMap.toMap(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname1", student1);
+        expected.put("Surname4", student2);
+        expected.put("Surname5", student3);
+        assertThat(rsl, is(expected));
+    }
+}
