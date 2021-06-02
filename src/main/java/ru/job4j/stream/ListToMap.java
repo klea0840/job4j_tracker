e30a17cd87044c56c996e2064eb9aa9d7d7d7e74
Lalina Kairova <klea0840@gmail.com>
2021-06-02 21:06:54 +0300

    4. Преобразование List в Map. [#110226 #162187] corr

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -2,21 +2,20 @@ package ru.job4j.stream;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ListToMap {
 
     public Map<String, Student> toMap(List<Student> list) {
-        Predicate<Student> containsDplt = student -> list.contains(student.getSurname());
         return list.stream().collect(
                 Collectors.toMap(
                         Student::getSurname,
-                        s -> {
-                            if (!containsDplt.test(s)) {
-                                list.add(s);
+                        s -> s,
+                        (element, dublicate) -> {
+                            if (!element.equals(dublicate)) {
+                                list.add(element);
                             }
-                            return s;
+                            return element;
                         }
                 ));
     }
--- src/test/java/ru/job4j/stream/ListToMapTest.java
+++ src/test/java/ru/job4j/stream/ListToMapTest.java
@@ -26,4 +26,24 @@ public class ListToMapTest {
         expected.put("Surname5", student3);
         assertThat(rsl, is(expected));
     }
+
+    @Test
+    public void testToMap2() {
+        Student student1 = new Student(0, "");
+        Student student2 = new Student(40, "Surname4");
+        Student student3 = new Student(50, "Surname5");
+        Student student5 = new Student(60, "Surname7");
+        Student student4 = new Student(50, "Surname5");
+        List<Student> students = List.of(
+                student1, student2, student3, student4, student5
+        );
+        ListToMap newMap = new ListToMap();
+        Map<String, Student> rsl = newMap.toMap(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("", student1);
+        expected.put("Surname4", student2);
+        expected.put("Surname5", student3);
+        expected.put("Surname7", student5);
+        assertThat(rsl, is(expected));
+    }
 }
