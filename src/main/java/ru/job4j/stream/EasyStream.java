b4edf5a6e67de402c3439ef83f015757160dfe32
Lalina Kairova <klea0840@gmail.com>
2021-06-22 13:46:12 +0300

    Дополнительная задача Stream

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,44 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+    private List<Integer> source;
+
+    public EasyStream(List<Integer> source) {
+        this.source = source;
+        }
+
+    public List<Integer> getSource() {
+        return this.source;
+    }
+
+    public static EasyStream of(List<Integer> source) {
+        return new EasyStream(source);
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> list = new ArrayList<>();
+        for (Integer i: this.source) {
+             list.add(fun.apply(i));
+        }
+        return new EasyStream(list);
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        List<Integer> list = new ArrayList<>();
+        for (Integer i: this.source) {
+            if (fun.test(i)) {
+                list.add(i);
+            }
+        }
+        return new EasyStream(list);
+    }
+
+    public List<Integer> collect() {
+        return new EasyStream(this.source).getSource();
+    }
+}
